6c29a12cff09466e535f5a0be1064db7c760f2b7
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2021-11-02 21:12:03 +0000

    fix build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,22 @@
 import typescript from '@rollup/plugin-typescript';
 
-export default {
-    input: 'src/index.ts',
-    output: [
-        {
-            file: 'dist/index.js',
-            format: 'cjs'
+export default [
+    {
+        input: 'src/index.ts',
+        output: {
+            dir: 'dist',
+            format: 'cjs',
+            sourcemap: true
         },
-        {
+        plugins: [typescript()]
+    },
+    {
+        input: 'src/index.ts',
+        output: {
             file: 'dist/index.esm.js',
-            format: 'esm'
-        }
-    ],
-    plugins: [typescript()]
-};
+            format: 'esm',
+            sourcemap: true
+        },
+        plugins: [typescript()]
+    }
+];
