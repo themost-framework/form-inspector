16e7d5da81abe2adefd47d3ea2841c9a40de75c7
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2021-11-02 20:18:34 +0000

    initial commit

---
+++ src/FormInspector.spec.ts
@@ -0,0 +1,7 @@
+import { FormInspector } from "./index";
+describe('FormInspector', () => {
+    it('should create instance', () => {
+        const inspector = new FormInspector();
+        expect(inspector).toBeTruthy();
+    });
+});
---
+++ src/FormInspector.ts
@@ -0,0 +1,8 @@
+export class FormInspector {
+    constructor() {
+        //
+    }
+    inspect() {
+        
+    }
+}
